97477edc6fe37ef050888e43a62dd7e22442c1be
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-07-16 17:55:21 +0530

    pagination is next

--- client/src/components/newscard/newscard.js
+++ client/src/components/newscard/newscard.js
@@ -1,5 +1,10 @@
 import "./newscard.css";
 const NewsCard = ({ item, Key }) => {
+  //2021-01-09T12:15:30Z
+  const fulldate = new Date(item.publishedAt); // Sat  Jan 09 2021  17:45:30  GMT+0530
+  var date = fulldate.toString().split(" "); // ["Sat", "Jan", "09", "2021", "17:45:30", "GMT+0530"]
+  const hour = parseInt(date[4].substring(0, 2)); //
+  const time = hour > 12 ? true : false;
   return (
     <>
       <div className="newsCard">
@@ -16,14 +21,17 @@ const NewsCard = ({ item, Key }) => {
           <div>
             <span className="title">{item.title}</span>
             <br />
-            <span className="author">
-              <a href={item.url} target="_blank">
-                <span className="muted">
-                  {" "}
-                  by {item.author ? item.author : "unknown"}{" "}
-                </span>
-              </a>{" "}
-            </span>
+            <div className="dateauthor">
+              <p className="muted">
+                by {item.author ? item.author : "unknown"}
+              </p>
+              <p className="muted">
+                {time
+                  ? `${hour - 12}:${date[4].substring(3, 5)} pm`
+                  : `${hour}:${date[4].substring(3, 5)} am`}{" "}
+                on {date[2]} {date[1]} {date[3]}, {date[0]}
+              </p>
+            </div>
           </div>
           <div className="lowerNewsText">
             <div className="description">{item.description}</div>
